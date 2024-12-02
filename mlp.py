01640908e2e6021eae925badea699ee30fec5a3a
richard-c2411 <rcenedella@uri.edu>
2024-12-01 23:25:39 -0500

    Cleaned up the code in the mlp.py file. Changed the distribution of the test, validation, and training data. I fixed the accuracy and loss calculations to be correct. I also ran a lot of tests to see which hyperparameters would work the best. The results are in the _05_ notebook at the bottom. I also added pictures for a lot of the results in the test runs folder. Accuracy is really low so far unfortunately.

--- mlp.py
+++ mlp.py
@@ -4,28 +4,29 @@ from torchvision import datasets, transforms
 from torch.utils.data import SubsetRandomSampler
 import sys
 import os
+from sklearn.preprocessing import StandardScaler
 
 class MLP(nn.Module):
   def __init__(self, in_dim, out_dim, hid_layers):
     super(MLP, self).__init__()
-    self.nnLayers = [nn.Flatten()] #this will hold all the layers of the neural netowrk. Flatten is to make sure that the shapes are all the same to avoid any mismatches. 
+    self.nn_layers = [nn.Flatten()] #this will hold all the layers of the neural netowrk. Flatten is to make sure that the shapes are all the same to avoid any mismatches. 
     #input_dim = in_dim
     for i in range (len(hid_layers)):
       hidden_dim = hid_layers[i]
       if i == 0: #first input layer. This takes the specified input dimension and the dimension of first hidden layer
-        self.nnLayers.append(nn.Linear(in_dim, hidden_dim))
+        self.nn_layers.append(nn.Linear(in_dim, hidden_dim))
         
       else: #the hidden layers.
-        self.nnLayers.append(nn.Linear(input_dim, hidden_dim))
+        self.nn_layers.append(nn.Linear(input_dim, hidden_dim))
       #this will connect the layers together by storing the dimension of the output from the layer for the next layer.
       input_dim = hidden_dim
       #apply the relu activation function
-      self.nnLayers.append(nn.ReLU())
+      self.nn_layers.append(nn.ReLU())
       
     #output layer  
-    self.nnLayers.append(nn.Linear(input_dim, out_dim))
+    self.nn_layers.append(nn.Linear(input_dim, out_dim))
 
-    self.net = nn.Sequential(*self.nnLayers)
+    self.net = nn.Sequential(*self.nn_layers)
 
 
   def forward(self, x):
@@ -33,67 +34,59 @@ class MLP(nn.Module):
     return self.net(x)
   
   def test_model(self, model, criterion, loader, device):
-    chosenModel = model
-    chosenCriterion = criterion
-    chosenLoader = loader
-    chosenDevice = device
+    chosen_model = model
     
     #store the values of each batch in these variables
-    correctPredictions = 0
-    totalSamples = 0
-    totalLoss = 0.0
+    correct_predictions = 0
+    total_loss = 0.0
 
     #set the model to evaluation mode
-    chosenModel.eval()
+    chosen_model.eval()
     #disable gradient computation
     with torch.no_grad():
-        for batch in chosenLoader:
+        for i, batch in enumerate(loader):
             #first get the batch data
-            x = batch[0].to(chosenDevice) # input data(features)
-            y = batch[1].to(chosenDevice) # target labels that corresponds with the input data
-            logits = chosenModel(x)
+            x = batch[0].to(device) # input data(features)
+            y = batch[1].to(device) # target labels that corresponds with the input data
+            logits = chosen_model(x)
+            #calculate the loss. The outpus and the labels
+            batch_loss = criterion(logits,y)
+            
+            total_loss += (batch_loss.item() * x.size(0))
 
             #get the predictions so the accuracy can be calculated
             preds = torch.argmax(logits, dim=1)
             #calculate the accuracy of the current batch. .sum counts the number of correct predictions for the tensor (preds == y). That looks at all the predictions in the batch against the correct label. 
-            #add up all the total number of correct predictions per batch. This works because true predictions are value 1 and false are 0 so only 1's add up.
-            correctPredictions += ((preds == y).sum().item())
-            #calculate the loss. The outpus and the labels
-            batchLoss = chosenCriterion(logits,y)
-            
-            totalLoss += (batchLoss.item() * x.size(0))
+            correct_predictions += ((preds == y).sum().item() / x.size(0))
 
-            #this gets the total number of samples in this batch and adds it to the total number of samples. 
-            totalSamples += x.size(0)
-
-    #calculate the average accuracy accross all the batches by comparing the number of correct predictions to the total number of samples.        
-    averageAccuracy = correctPredictions / totalSamples
 
     #calculate the average loss
-    averageLoss = totalLoss / totalSamples
-    
-    return averageAccuracy, averageLoss
+    average_loss = total_loss / len(loader.dataset)
+    #calculate the average accuracy accross all the batches by comparing the number of correct predictions to the total number of samples.        
+    average_accuracy = correct_predictions / len(loader)
+
+    return average_loss, average_accuracy
   
+  #this will train the model on the data
   def train_model(self, model, criterion, optimizer, tr_loader, va_loader, n_epochs, device):
-    chosenModel = model
+    chosen_model = model
     
-    numberOfEpochs = n_epochs
-    trainingLoss = []
-    trainingAccuracy = []
-    validationLoss = []
-    validationAccuracy = []
+    training_loss = []
+    training_accuracy = []
+    validation_loss = []
+    validation_accuracy = []
     #go through each epoch
-    for epoch in range(numberOfEpochs):
+    for epoch in range(n_epochs):
         #set the model to train mode
-        chosenModel.train()
+        chosen_model.train()
 
         #for training data
-        epochLoss = 0
-        epochAccuracy = 0
+        training_epoch_loss = 0
+        training_epoch_accuracy = 0
 
         #for validation data
-        validationEpochLoss = 0
-        validationEpochAccuracy = 0
+        validation_epoch_loss = 0
+        validation_epoch_accuracy = 0
         
 
         #go through all the training minibatches
@@ -102,59 +95,55 @@ class MLP(nn.Module):
             x = batch[0].to(device)
             y = batch[1].to(device)
             #perform the forward pass
-            logits = chosenModel(x)
+            logits = chosen_model(x)
             #compute the loss of this batch
             loss = criterion(logits, y)
-            epochLoss += (loss.item()* x.size(0))
+            training_epoch_loss += (loss.item()* x.size(0))
             #compute the accuracy of the current batch
             preds = torch.argmax(logits, dim=1)
-            epochAccuracy += ((preds == y).sum() / x.size(0))
+            training_epoch_accuracy += ((preds == y).sum() / x.size(0))
             #backward pass
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         #calculate the accuracy and loss on the training data
-        currentTrainingEpochLoss = epochLoss/len(tr_loader.dataset)
-        trainingLoss.append(currentTrainingEpochLoss)
-        currentEpochAccuracy = epochAccuracy/len(tr_loader)
-        trainingAccuracy.append(currentEpochAccuracy)
+        current_training_epoch_loss = training_epoch_loss/len(tr_loader.dataset)
+        training_loss.append(current_training_epoch_loss)
+        current_epoch_accuracy = training_epoch_accuracy/len(tr_loader)
+        training_accuracy.append(current_epoch_accuracy)
 
-        chosenModel.eval()
+        chosen_model.eval()
         with torch.no_grad():
-            
-            #go through the batches of validation data
-            for i,batch in enumerate(va_loader):
-                x = batch[0].to(device) #features
-                y = batch[1].to(device) #y labels
-                logits = chosenModel(x)
+          #go through the batches of validation data
+          for i,batch in enumerate(va_loader):
+              x = batch[0].to(device) #features
+              y = batch[1].to(device) #y labels
+              logits = chosen_model(x)
 
-                #calculate the loss
-                validationBatchLoss = criterion(logits, y)
-                validationEpochLoss += (validationBatchLoss.item() * x.size(0))
-                #calculate the accuracy per batch
-                preds = torch.argmax(logits, dim=1)               
-                validationEpochAccuracy += ((preds == y).sum() / x.size(0))
-                
-
-                
+              #calculate the loss
+              validationbatch_loss = criterion(logits, y)
+              validation_epoch_loss += (validationbatch_loss.item() * x.size(0))
+              #calculate the accuracy per batch
+              preds = torch.argmax(logits, dim=1)               
+              validation_epoch_accuracy += ((preds == y).sum() / x.size(0))
+                              
         #calculate the accuracy and loss on the validation data
-        currentValidationEpochLoss = validationEpochLoss/len(va_loader.dataset)
-        validationLoss.append(currentValidationEpochLoss)
-        currentValidationEpochAccuracy = validationEpochAccuracy / len(va_loader)
-        validationAccuracy.append(currentValidationEpochAccuracy)
+        current_validation_epoch_loss = validation_epoch_loss/len(va_loader.dataset)
+        validation_loss.append(current_validation_epoch_loss)
+        current_validation_epoch_accuracy = validation_epoch_accuracy / len(va_loader)
+        validation_accuracy.append(current_validation_epoch_accuracy)
 
 
         #print out the results at the end of each epoch
-        print(f'Epoch {epoch}, training Loss { currentTrainingEpochLoss}, training Accuracy {currentEpochAccuracy}, validation loss {currentValidationEpochLoss}, validation accuracy {currentValidationEpochAccuracy}')
+        print(f'Epoch {epoch}, training Loss { current_training_epoch_loss}, training Accuracy {current_epoch_accuracy}, validation loss {current_validation_epoch_loss}, validation accuracy {current_validation_epoch_accuracy}')
 
     #return the accuracy and loss lists
-    return trainingLoss, trainingAccuracy, validationLoss, validationAccuracy
+    return training_loss, training_accuracy, validation_loss, validation_accuracy
   
+
   def load_model_dataset(self, batch_size):
-    #download
-    #dataTr = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
-    #dataVa = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+    #this will load the current working directory
     repository_root_directory = os.path.dirname(os.getcwd())
     rrd = "repository_root_directory:\t"
     print(rrd, repository_root_directory)
@@ -165,10 +154,9 @@ class MLP(nn.Module):
     else:  
         print(rrd, "already in path")
 
+    #extract the features from the dataset and save them as features and labels
     from features_extractor import FeaturesExtractor
-    import numpy as np
-
-
+    
     preprocessed_dataset = "../_02_data_preprocessed"
     extractor = FeaturesExtractor()
     data = extractor.extract_features_all_files(preprocessed_dataset)
@@ -176,58 +164,44 @@ class MLP(nn.Module):
     features = torch.tensor(data.drop('Genre', axis=1).values, dtype=torch.float32)
     labels = torch.tensor(data['Genre'].values, dtype=torch.long)
 
-    #create tensordataset
+    #create tensor dataset
     from torch.utils.data import TensorDataset
-    tensorData = TensorDataset(features, labels)
+    tensor_data = TensorDataset(features, labels)
 
-    #features = np.array(data['features'])
-    #labels = np.array(data['labels'])
-    #print("Shape of the deata", features.shape)
-    #print("Number of features: ",features.shape[1])
-    #print("Number of datapoints: ", features.shape[0])
-
-    #size of the subset
-    #subSetSize = 10000
-
-    #choose the random datapoints from the larger dataset
-    #subsetIindices = torch.randperm(len(dataTr))[:subSetSize]
-    #subSetDataTr = torch.utils.data.Subset(dataTr, subsetIindices)
     
     #split validation into validation and test data
-    dataTr, dataTe, dataVa = torch.utils.data.random_split(tensorData, [.25, .25, .5])
-    #check the classes of the data
-    #print(dataTr.classes)
-    
+    data_tr, data_te, data_va = torch.utils.data.random_split(tensor_data, [.7, .15, .15])
+
     #create the dataloaders
     bSize = batch_size
     
-    trainLoader = torch.utils.data.DataLoader(dataTr, batch_size=bSize, shuffle=True)
-    validLoader = torch.utils.data.DataLoader(dataVa, batch_size=len(dataVa), shuffle=False)
-    testLoader = torch.utils.data.DataLoader(dataTe, batch_size=len(dataTe), shuffle=False)
+    train_loader = torch.utils.data.DataLoader(data_tr, batch_size=bSize, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(data_va, batch_size=len(data_va), shuffle=False)
+    test_loader = torch.utils.data.DataLoader(data_te, batch_size=len(data_te), shuffle=False)
 
     #get the shape of each dataloader
-    for batch in trainLoader:
-        sizeOfTrain = len(trainLoader.dataset)
-        featureShape = batch[0].shape
-        labelShape = batch[1].shape
-        trainLoaderInfo = f"[{sizeOfTrain},{featureShape},  {labelShape}]"
+    for batch in train_loader:
+        size_of_train = len(train_loader.dataset)
+        feature_shape = batch[0].shape
+        label_shape = batch[1].shape
+        train_loader_info = f"[{size_of_train},{feature_shape},  {label_shape}]"
         break
         
 
-    for batch in validLoader:
-        featureShape = batch[0].shape
-        labelShape = batch[1].shape
-        validLoaderInfo = f"[{featureShape},  {labelShape}]"
+    for batch in valid_loader:
+        feature_shape = batch[0].shape
+        label_shape = batch[1].shape
+        valid_loader_info = f"[{feature_shape},  {label_shape}]"
         break
 
-    for batch in testLoader:
-        featureShape = batch[0].shape
-        labelShape = batch[1].shape
-        testLoaderInfo = f"[{featureShape},  {labelShape}]"
+    for batch in test_loader:
+        feature_shape = batch[0].shape
+        label_shape = batch[1].shape
+        test_loader_info = f"[{feature_shape},  {label_shape}]"
         break
 
     #print out the dataset info
-    print("shape of train: ", trainLoaderInfo, "shape of validation: ", validLoaderInfo, "shape of test: ", testLoaderInfo)
+    print("shape of train: ", train_loader_info, "shape of validation: ", valid_loader_info, "shape of test: ", test_loader_info)
 
     #return the dataloaders
-    return trainLoader, validLoader, testLoader
+    return train_loader, valid_loader, test_loader
